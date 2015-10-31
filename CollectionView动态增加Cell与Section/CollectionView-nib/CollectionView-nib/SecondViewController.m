e2f39f132a1a83da8cb63f3fe0c4a8a45ae7eec4
chenyufeng <503434355@qq.com>
2015-10-31 23:42:52 +0800

    add

---
+++ CollectionView动态增加Cell与Section/CollectionView-nib/CollectionView-nib/SecondViewController.m
@@ -0,0 +1,216 @@
+
+//
+//  SecondViewController.m
+//  CollectionView-nib
+//
+//  Created by chenyufeng on 15/10/30.
+//  Copyright © 2015年 chenyufengweb. All rights reserved.
+//
+
+#import "SecondViewController.h"
+#import "CollectionViewCell.h"
+#import "AppDelegate.h"
+#import "CollectionReusableView.h"
+
+#define UISCREEN_WIDTH ([[UIScreen mainScreen] bounds].size.width)
+
+@interface SecondViewController ()<UICollectionViewDataSource,UICollectionViewDelegate,UICollectionViewDelegateFlowLayout>
+
+@property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
+
+//开始使用二维数组进行数据结构设计；
+@property (nonatomic, strong)NSArray *defaultArray;//有defaultArray()方法；
+@property (nonatomic, strong)NSMutableArray *dataArray;//有一个方法的名称也是dataArray();
+
+//下面数组用来存放头部标题；
+@property(strong,nonatomic) NSMutableArray *headerArray;
+
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  
+  //进行CollectionView和Cell的绑定
+  [self.collectionView registerClass:[CollectionViewCell class]  forCellWithReuseIdentifier:@"CollectionCell"];
+  self.collectionView.backgroundColor = [UIColor whiteColor];
+  
+  //加入头部视图；
+  [self.collectionView registerClass:[CollectionReusableView class] forSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:@"Header"];
+  
+}
+
+
+
+#pragma mark - UICollectionViewDataSource
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
+  
+  return [[self.dataArray objectAtIndex:section] count];
+}
+
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
+  
+  CollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"CollectionCell" forIndexPath:indexPath];
+  cell.imageView.image = [UIImage imageNamed:[[self.dataArray objectAtIndex:indexPath.section] objectAtIndex:indexPath.row]];
+  return cell;
+  
+}
+
+//有多少个section；
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView{
+  //有多少个一维数组；
+  return self.dataArray.count;
+}
+
+//加载头部标题；
+- (UICollectionReusableView *)collectionView:(UICollectionView *)collectionView viewForSupplementaryElementOfKind:(NSString *)kind atIndexPath:(NSIndexPath *)indexPath{
+  
+  CollectionReusableView *view = [collectionView dequeueReusableSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:@"Header" forIndexPath:indexPath];
+  
+  //就打印出当前section的值；
+  view.title.text = [self.headerArray objectAtIndex:indexPath.section];
+  
+  return view;
+  
+}
+
+
+#pragma mark - UICollectionViewDelegateFlowLayout
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath{
+  
+  return CGSizeMake((UISCREEN_WIDTH - 40) / 3, (UISCREEN_WIDTH - 40) / 3);
+}
+
+- (UIEdgeInsets)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout insetForSectionAtIndex:(NSInteger)section{
+  
+  return UIEdgeInsetsMake(0,20,0,20);
+}
+
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout referenceSizeForHeaderInSection:(NSInteger)section{
+  
+  return CGSizeMake(self.collectionView.frame.size.width, 50);
+}
+
+- (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout minimumInteritemSpacingForSectionAtIndex:(NSInteger)section{
+  
+  return 0;
+}
+
+- (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout minimumLineSpacingForSectionAtIndex:(NSInteger)section{
+  
+  return 0;
+}
+
+#pragma mark -- Lazy Load 懒加载
+//要添加的图片从这里面选；
+//这里进行的是懒加载，要先去判断，没有的话才去进行实例化；
+- (NSArray *)defaultArray{
+  if (!_defaultArray){
+    
+    NSArray *firstRowDefault = [[NSArray alloc] initWithObjects:@"0",@"1",@"2",@"3",@"4",nil];
+    NSArray *secondRowDefault = [[NSArray alloc] initWithObjects:@"0",@"1",@"2",@"3",@"4",nil];
+    
+    self.defaultArray = [[NSArray alloc] initWithObjects:firstRowDefault,secondRowDefault, nil];
+  }
+  return _defaultArray;
+}
+
+- (NSMutableArray *)dataArray{
+  if (!_dataArray){
+    
+    NSMutableArray *firstRow = [[NSMutableArray alloc] initWithObjects:@"0",@"1",@"2",@"3",@"4",@"5",nil];
+    NSMutableArray *secondRow = [[NSMutableArray alloc] initWithObjects:@"0",@"1",@"2",@"3",@"4",@"5",nil];
+    
+    self.dataArray = [[NSMutableArray alloc] initWithObjects:firstRow,secondRow, nil];
+    
+  }
+  
+  return _dataArray;
+}
+
+//这里标题的添加也使用懒加载；
+- (NSMutableArray *)headerArray{
+  
+  if (!_headerArray) {
+    self.headerArray = [[NSMutableArray alloc] initWithObjects:@"第一个",@"第二个", nil];
+  }
+  
+  return _headerArray;
+}
+
+
+#pragma mark - UICollectionViewDelegat
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath{
+  
+  if ((indexPath.row == ([[self.dataArray objectAtIndex:indexPath.section] count] - 1))) {
+    NSLog(@"添加操作");
+    
+    NSString *tempImageName = @"1";
+    
+    //这里需要在最后一个位置增加设备；
+    
+    [[self.dataArray objectAtIndex:indexPath.section] insertObject:tempImageName atIndex:[[self.dataArray objectAtIndex:indexPath.section] count]-1];
+    
+    [self.collectionView reloadData];
+    
+  }else{
+    NSLog(@"第%ld个section,点击图片%ld",indexPath.section,indexPath.row);
+  }
+  
+}
+
+#pragma mark - 添加环境的按钮
+- (IBAction)addEnvirnmentClick:(id)sender {
+  
+  NSMutableArray *addRow = [[NSMutableArray alloc] initWithObjects:@"0",@"1",@"2",@"3",@"4",@"5",nil];
+  
+  [self.dataArray insertObject:addRow atIndex:[self.dataArray count]];
+  //  [self.collectionView reloadData];
+  
+  [self popEnvirnmentNameDialog];
+  
+}
+
+#pragma mark - 弹出输入环境名称的提示框
+- (void)popEnvirnmentNameDialog{
+  
+  UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"提示" message:@"请输入Section名称" preferredStyle:UIAlertControllerStyleAlert];
+  //以下方法就可以实现在提示框中输入文本；
+  [alertController addAction:[UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+    UITextField *envirnmentNameTextField = alertController.textFields.firstObject;
+    
+    //插入到headerArray数组中
+    [self.headerArray insertObject:envirnmentNameTextField.text atIndex:self.headerArray.count];
+    
+    //此时更新界面；
+    [self.collectionView reloadData];
+    
+    NSLog(@"你输入的文本%@",envirnmentNameTextField.text);
+    
+  }]];
+  
+  [alertController addAction:[UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleDefault handler:nil]];
+  [alertController addTextFieldWithConfigurationHandler:^(UITextField * _Nonnull textField) {
+    
+    textField.placeholder = @"请输入Section名称";
+  }];
+  [self presentViewController:alertController animated:true completion:nil];
+  
+}
+
+
+@end
+
+
+
+
+
+
+
+
+
+
+
--- README.md
+++ README.md
@@ -2,7 +2,18 @@
 &emsp;&emsp;&nbsp;这里我在iOS中使用三种不同的方式实现UICollectionView，也就是一种瀑布流视图。分别使用storyboard、nib和纯代码的方式来实现。
 ##1.项目说明
 &emsp;&emsp;&nbsp;UICollectionView继承自UITableView，基本的实现方式同UITableView。用UICollectionView实现的视图就是所谓的瀑布流界面，很多的电商网站，图片浏览App都是采用了这种设计方式。在我的这个项目中，我分别使用三种不同的方式来实现CollectionView：storyboard、nib文件和纯代码，可以适用在多种不同的项目开发情境下。
-##2.实现效果
+##2.动态加载
+&emsp;&emsp;&nbsp;我在第四个例子中实现了使用UICollectionView进行动态增加一个Cell或者Section，主要使用了Lazy Load懒加载进行。并在插入一个Section的时候动态编辑Header头部视图。
+##3.实现效果
 ![Alt text](https://github.com/chenyufeng1991/CollectionView/raw/master/Screenshots/1.png)
-##3.技术博客
+&emsp;&emsp;&nbsp;动态增加Cell和Section的效果如下：</br>
+![Alt text](https://github.com/chenyufeng1991/CollectionView/raw/master/Screenshots/2.png)
+</br>
+![Alt text](https://github.com/chenyufeng1991/CollectionView/raw/master/Screenshots/3.png)
+</br>
+![Alt text](https://github.com/chenyufeng1991/CollectionView/raw/master/Screenshots/4.png)
+
+
+
+##4.技术博客
 我的个人技术博客：[http://blog.csdn.net/chenyufeng1991](http://blog.csdn.net/chenyufeng1991) 。欢迎大家访问！
