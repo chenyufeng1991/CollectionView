d89d3921a4cc065fe920b348249703049192ac10
chenyufeng <503434355@qq.com>
2015-11-25 10:16:22 +0800

    add

---
+++ 动态增加cell和section实现/CollectionView-nib/CollectionFirstSectionReusableView.m
@@ -0,0 +1,42 @@
+//
+//  CollectionFirstSectionReusableView.m
+//  CollectionView-nib
+//
+//  Created by chenyufeng on 15/11/24.
+//  Copyright © 2015年 chenyufengweb. All rights reserved.
+//
+
+#import "CollectionFirstSectionReusableView.h"
+
+#define SCREEN_WIDTH ([[UIScreen mainScreen] bounds].size.width)
+#define SCREEN_HEIGHT ([[UIScreen mainScreen] bounds].size.height)
+
+@implementation CollectionFirstSectionReusableView
+
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+  self = [super initWithFrame:frame];
+  if (self) {
+    
+    self.title = [[UILabel alloc] initWithFrame:CGRectMake(0, 155, SCREEN_WIDTH, 20)];
+    self.title.textColor = [UIColor blackColor];
+    self.title.textAlignment = NSTextAlignmentCenter;
+    
+    
+    
+    self.imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, [[UIScreen mainScreen] bounds].size.width, 150)];
+    [self.imageView setImage:[UIImage imageNamed:@"home"]];
+    
+    
+    
+    
+    [self addSubview:self.title];
+    [self addSubview:self.imageView];
+    
+    
+  }
+  return self;
+}
+
+@end
---
+++ 动态增加cell和section实现/CollectionView-nib/CollectionViewCell.h
@@ -0,0 +1,18 @@
+//
+//  CollectionViewCell.h
+//  CollectionView-nib
+//
+//  Created by chenyufeng on 15/10/30.
+//  Copyright © 2015年 chenyufengweb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CollectionViewCell : UICollectionViewCell
+
+@property(strong,nonatomic) UIImageView *imageView;
+@property(strong,nonatomic) UILabel *descLabel;
+
+@end
+
+
