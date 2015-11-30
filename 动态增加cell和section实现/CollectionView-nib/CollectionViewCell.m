4c717c59abea27c3ba99ecb1f15115cc17fa0211
chenyufeng <503434355@qq.com>
2015-11-30 22:56:23 +0800

    dev:实现cell的动态增加、删除，以及section的增加；并使用模型实现。

--- 动态增加cell和section实现/CollectionView-nib/CollectionViewCell.m
+++ 动态增加cell和section实现/CollectionView-nib/CollectionViewCell.m
@@ -9,6 +9,7 @@
 #import "CollectionViewCell.h"
 
 #define UISCREEN_WIDTH ([[UIScreen mainScreen] bounds].size.width)
+#define CELL_WIDTH (([[UIScreen mainScreen] bounds].size.width - 40) / 3)
 
 @implementation CollectionViewCell
 
@@ -20,15 +21,24 @@
 
     //这里需要初始化ImageView；
     self.imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, (UISCREEN_WIDTH - 40) / 3, (UISCREEN_WIDTH - 40) / 3)];
-    [self addSubview:self.imageView];
     [self.imageView setUserInteractionEnabled:true];
     
     
     self.descLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, (UISCREEN_WIDTH - 40) / 3, (UISCREEN_WIDTH - 40) / 3, 20)];
     self.descLabel.textAlignment = NSTextAlignmentCenter;
     
-    [self addSubview:self.descLabel];
     
+    
+    self.deleteButton = [[UIButton alloc] initWithFrame:CGRectMake(CELL_WIDTH - 35, 5, 30, 30)];
+    [self.deleteButton setImage:[UIImage imageNamed:@"close"] forState:UIControlStateNormal];
+    //先设置不可见；
+    [self.deleteButton setHidden:true];
+    
+    
+    
+    [self addSubview:self.imageView];
+    [self addSubview:self.descLabel];
+    [self addSubview:self.deleteButton];
     
   }
   return self;
